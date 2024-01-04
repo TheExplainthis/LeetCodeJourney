26d09545ddc4eddf3b500ff7e94c1ff785f10ed8
TheExplainthis <hello@explainthis.io>
2024-01-04 13:19:20 +0800

    Update

--- solutions/21. Merge Two Sorted Lists/approach1.py
+++ solutions/21. Merge Two Sorted Lists/approach1.py
@@ -5,14 +5,13 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        curr = dummy = ListNode(-1)
-        while l1 and l2:
-            if l1.val < l2.val:
-                curr.next = l1
-                l1 = l1.next
-            else:
-                curr.next = l2
-                l2 = l2.next
-            curr = curr.next
-        curr.next = l1 or l2
-        return dummy.next
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
--- solutions/21. Merge Two Sorted Lists/approach2.py
+++ solutions/21. Merge Two Sorted Lists/approach2.py
@@ -5,13 +5,14 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if l1 is None:
-            return l2
-        elif l2 is None:
-            return l1
-        elif l1.val < l2.val:
-            l1.next = self.mergeTwoLists(l1.next, l2)
-            return l1
-        else:
-            l2.next = self.mergeTwoLists(l1, l2.next)
-            return l2
+        curr = dummy = ListNode(-1)
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 or l2
+        return dummy.next
