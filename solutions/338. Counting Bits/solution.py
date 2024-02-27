9478f9365d852fc67097fd1f3f337eb7044e50b6
TheExplainthis <hello@explainthis.io>
2024-02-27 10:28:46 +0800

    Add 338

---
+++ solutions/338. Counting Bits/solution.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        result = [0] * (n + 1)
+        for i in range(1, n + 1):
+            result[i] = result[i & (i - 1)] + 1
+        return result
